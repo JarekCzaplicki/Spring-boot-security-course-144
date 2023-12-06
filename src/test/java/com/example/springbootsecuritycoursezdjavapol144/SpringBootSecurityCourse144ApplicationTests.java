d8c6a5755627e97d064e496a8a2e24e1295d14d6
jarekczaplicki <jarekczaplicki@tlen.pl>
2023-12-06 18:41:30 +0100

    Initializing project

---
+++ src/test/java/com/example/springbootsecuritycoursezdjavapol144/SpringBootSecurityCourse144ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootsecuritycoursezdjavapol144;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityCourse144ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
